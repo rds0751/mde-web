e721a3620b7c5cf2e6e53524376e6823ae7c26c9
Ritik <ritiktyagi01356@gmail.com>
2022-10-10 11:47:16 +0530

    test commit

---
+++ native/views.py
@@ -0,0 +1,245 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'native/front-pagec.html')
+
+
+def add_member(request):
+    return render(request, 'native/add-member.html')
+
+
+
+def bp_and_sugar_control(request):
+    return render(request, 'native/bp-and-sugar-control.html')
+
+
+
+def contact_medose(request):
+    return render(request, 'native/contact-medose.html')
+
+
+
+
+def daily_dairy_icon(request):
+    return render(request, 'native/daily-dairy-icon.html')
+
+
+
+def find_your_specialist_doctors(request):
+    return render(request, 'native/find-your-specialist-doctors.html')
+
+
+
+def health_tips_icon(request):
+    return render(request, 'native/health-tips-icon.html')
+
+
+
+def hospital_reports(request):
+    return render(request, 'native/hospital-reports.html')
+
+
+
+def iphone_xr_xs_max_11_1(request):
+    return render(request, 'native/iphone-xr-xs-max-11-1.html')
+
+
+
+def lab_test(request):
+    return render(request, 'native/lab-test.html')
+
+
+
+def link_at_dairy_doctor(request):
+    return render(request, 'native/link-at-dairy-doctor.html')
+
+
+
+def login(request):
+    return render(request, 'native/login.html')
+
+
+
+def medicine_reminder(request):
+    return render(request, 'native/medicine-reminder.html')
+
+
+
+def medose_join_now_menu_page(request):
+    return render(request, 'native/medose-join-now-menu-page.html')
+
+
+
+def menu_icon_1(request):
+    return render(request, 'native/menu-icon-1.html')
+
+
+
+def my_activity_page_2(request):
+    return render(request, 'native/my-activity-page-2.html')
+
+
+
+def my_activity(request):
+    return render(request, 'native/my-activity.html')
+
+
+
+def my_cart(request):
+    return render(request, 'native/my-cart.html')
+
+
+
+def my_consulation(request):
+    return render(request, 'native/my-consulation.html')
+
+
+
+def my_device(request):
+    return render(request, 'native/my-device.html')
+
+
+
+def my_doctors_1(request):
+    return render(request, 'native/my-doctors-1.html')
+
+
+
+def my_doctors(request):
+    return render(request, 'native/my-doctors.html')
+
+
+
+def my_health(request):
+    return render(request, 'native/my-health.html')
+
+
+
+def my_orders(request):
+    return render(request, 'native/my-orders.html')
+
+
+
+def my_profile(request):
+    return render(request, 'native/my-profile.html')
+
+
+
+def my_record(request):
+    return render(request, 'native/my-record.html')
+
+
+
+def my_rewards(request):
+    return render(request, 'native/my-rewards.html')
+
+
+
+def notification(request):
+    return render(request, 'native/notification.html')
+
+
+
+def ocr_reader(request):
+    return render(request, 'native/ocr-reader.html')
+
+
+
+def plus_button_on_front_page_1(request):
+    return render(request, 'native/plus-button-on-front-page-1.html')
+
+
+
+def plus_button_on_front_page(request):
+    return render(request, 'native/plus-button-on-front-page.html')
+
+
+
+def prescription(request):
+    return render(request, 'native/prescription.html')
+
+
+
+def privacy_policy(request):
+    return render(request, 'native/privacy-policy.html')
+
+
+
+def record_notification(request):
+    return render(request, 'native/record-notification.html')
+
+
+
+def refer_and_earn(request):
+    return render(request, 'native/refer-and-earn.html')
+
+
+
+def settings(request):
+    return render(request, 'native/settings.html')
+
+
+
+def terms_and_conditions_menu(request):
+    return render(request, 'native/terms-and-conditions-menu.html')
+
+
+
+def terms_conditions_2(request):
+    return render(request, 'native/terms-conditions-2.html')
+
+
+
+def upload_new_prescriptions(request):
+    return render(request, 'native/upload-new-prescriptions.html')
+
+
+
+def using_guide_line_video(request):
+    return render(request, 'native/using-guide-line-video.html')
+
+
+def book_consultation(request):
+    return render(request, 'native/book-consultation.html')
+
+
+def past_consultation(request):
+    return render(request, 'native/past-consultation.html')
+
+def add_mydoctor(request):
+    return render(request, 'native/add-mydoctor.html')
+
+
+def lab_tests(request):
+    return render(request, 'native/lab-tests.html')
+
+def plusicon(request):
+    return render(request, 'native/plusicon.html')
+
+    
+def ambulance_page(request):
+    return render(request, 'native/ambulance-page.html')
+
+def bloodbank_page(request):
+    return render(request, 'native/bloodbank-page.html')
+
+
+def mydevice_page(request):
+    return render(request, 'native/my-device-page.html')
+
+
+def health_welness_page(request):
+    return render(request, 'native/health-welness-page.html')
+
+
+def lab_test_page(request):
+    return render(request, 'native/lab-test-page.html')
+
+
+def medicine_page(request):
+    return render(request, 'native/medicine-page.html')
+
+
+def subscription_page(request):
+    return render(request, 'native/subscription-page.html')
